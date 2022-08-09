0fca4f17f00e46471ae4c5e94b3399c3c349a53b
BartoszGrabowski1 <heksargonh@gmail.com>
2022-08-09 12:41:21 +0200

    New board look

--- src/main/java/utilities/Display.java
+++ src/main/java/utilities/Display.java
@@ -1,8 +1,23 @@
 package utilities;
 
 import board.Square;
+import board.SquareStatus;
 
 public class Display {
+
+    public final String LIGHT_BLUE = "\033[38;2;120;172;255m"; //LIGHT BLUE
+    public final String RESET = "\033[0m";  // Text Reset
+    public final String BLACK = "\033[0;30m";   // BLACK
+    public final String DARK_RED = "\033[38;2;145;40;16m"; //DARK RED
+    public final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
+    private final String SHIP = "\uD83D\uDEF3";
+    private final String WATER = "\uD83C\uDF0A";
+    private final String SHIP_SUNK = "\uD83D\uDCA3";
+    private final String HIT_MISS = "\uD83C\uDFAF";
+    private final String HIT_SHIP = "\uD83D\uDE80";
+
+
+
 
     public void welcomeMenu() {
         System.out.println("Welcome in BattleShip game!");
@@ -11,15 +26,30 @@ public class Display {
 
 
     public void printBoard(Square[][] ocean) {
-        System.out.println(".| 1 2 3 4 5 6 7 8 9 10");
+        System.out.println(BLACK_BACKGROUND_BRIGHT+".|  1   2   3  4   5  6   7   8   9  10 " +RESET +"|");
         for (int i = 0; i < ocean.length; i++) {
-            System.out.print((char) (i + 'A') + "| ");
+            System.out.print(BLACK_BACKGROUND_BRIGHT+(char) (i + 'A') + "|"+RESET+"  ");
             for (int j = 0; j < ocean.length; j++) {
-                System.out.print(ocean[i][j] + " ");
+                if (ocean[i][j].getSquareStatus().equals(SquareStatus.EMPTY)){
+                    System.out.print(LIGHT_BLUE + WATER + "  ");
+                } else if (ocean[i][j].getSquareStatus().equals(SquareStatus.SHIP)){
+                    System.out.print(SHIP + "  ");
+                } else if (ocean[i][j].getSquareStatus().equals(SquareStatus.MISSED)){
+                    System.out.print(HIT_MISS + "  ");
+                } else if (ocean[i][j].getSquareStatus().equals(SquareStatus.HIT)){
+                    System.out.print(DARK_RED+HIT_SHIP + "  ");
+                }
             }
-            System.out.println("");
+            System.out.println(RESET+"|");
 
         }
+        System.out.println("----------------------------------------/");
+        System.out.print(SHIP + RESET +" Ship ");
+        System.out.print(LIGHT_BLUE+WATER + RESET +" Water ");
+        System.out.println(BLACK+SHIP_SUNK + RESET +" Ship sunk ");
+        System.out.print(HIT_MISS + " Hit(Miss) ");
+        System.out.print(DARK_RED+HIT_SHIP + RESET +" Hit(Ship)");
+
     }
 
     public void consolePrint(String message){

9accfcd09f94aea70c74b0f3d8558a2907d792a5
BartoszGrabowski1 <heksargonh@gmail.com>
2022-08-09 09:40:54 +0200

    Simple board display and coordinates input validator

--- src/main/java/Battleship.java
+++ src/main/java/Battleship.java
@@ -1,9 +1,17 @@
-import board.Board;
+import board.*;
+import utilities.Display;
+import utilities.Input;
 
 public class Battleship {
     public static void main(String[] args) {
         Board board = new Board();
+        Display printer = new Display();
+        printer.welcomeMenu();
+        Square[][] ocean = board.getOcean();
+        printer.printBoard(ocean);
+        Input input = new Input();
+        input.getUserShot();
 
-//  commit test2
+
     }
 }

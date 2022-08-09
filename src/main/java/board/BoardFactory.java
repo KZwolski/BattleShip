26a49afd58e926751ca2306edac7b321223b6a85
KZwolski <kkzwolski@gmail.com>
2022-08-09 10:29:33 +0200

    Move methods from BoardFactory to input

--- src/main/java/board/BoardFactory.java
+++ src/main/java/board/BoardFactory.java
@@ -1,13 +1,10 @@
 package board;
 
-import java.util.Locale;
 import java.util.Objects;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 import ship.ShipType;
-import utilities.Display;
 import utilities.Input;
 
 public abstract class BoardFactory {
@@ -26,12 +23,12 @@ public abstract class BoardFactory {
 
     public void manualPlacement(Board board, ShipType shipType) {
         System.out.println("You are now placing: " + shipType.toString() + " of length: " + shipType.getLength());
-        int[] coordinates = getCoordinates();
-        String direction = getDirection();
+        int[] coordinates = input.getCoordinates();
+        String direction = input.getDirection();
         while (checkAreThereShips(shipType.getLength(), direction, board, coordinates)) {
             System.out.println("You cannot place ship there :(, try again! ");
-            coordinates = getCoordinates();
-            direction = getDirection();
+            coordinates = input.getCoordinates();
+            direction = input.getDirection();
         }
         markSquareAsShip(shipType.getLength(), direction, board, coordinates);
 
@@ -56,9 +53,6 @@ public abstract class BoardFactory {
 
     }
 
-    public boolean checkIfValidCords(int x, int y) {
-        return x < 10 && x >= 0 && y < 10 && y >= 0;
-    }
 
     public boolean checkAreThereShips(int shipSize, String direction, Board board, int[] coordinates) {
         int x = coordinates[0];
@@ -69,8 +63,8 @@ public abstract class BoardFactory {
             } else if (direction == "H") {
                 y += i;
             }
-            if (checkIfValidCords(x, y)) {
-                if (checkEveryDirection(board, x, y)) {
+            if (input.validateCords(x, y)) {
+                if (checkEveryDirectionNeighbours(board, x, y)) {
                     return true;
                 }
             } else {
@@ -80,54 +74,30 @@ public abstract class BoardFactory {
         return false;
     }
 
-    public boolean checkEveryDirection(Board board, int x, int y) {
+    public boolean checkEveryDirectionNeighbours(Board board, int x, int y) {
         return checkEdgeNeighbours(board, x, y) || checkHorizontalNeighbours(board, x, y) || checkVerticalNeighbours(board, x, y);
     }
 
     public boolean checkVerticalNeighbours(Board board, int x, int y) {
         Square[][] ocean = board.getOcean();
-        return (checkIfValidCords(x - 1, y) && Objects.equals(ocean[x - 1][y].getSquareStatus().toString(), "SHIP"))
-                || (checkIfValidCords(x + 1, y) && Objects.equals(ocean[x + 1][y].getSquareStatus().toString(), "SHIP"));
+        return (input.validateCords(x - 1, y) && Objects.equals(ocean[x - 1][y].getSquareStatus().toString(), "SHIP"))
+                || (input.validateCords(x + 1, y) && Objects.equals(ocean[x + 1][y].getSquareStatus().toString(), "SHIP"));
     }
 
     public boolean checkHorizontalNeighbours(Board board, int x, int y) {
         Square[][] ocean = board.getOcean();
-        return (checkIfValidCords(x, y - 1) && Objects.equals(ocean[x][y - 1].getSquareStatus().toString(), "SHIP"))
-                || (checkIfValidCords(x, y + 1) && Objects.equals(ocean[x][y + 1].getSquareStatus().toString(), "SHIP"));
+        return (input.validateCords(x, y - 1) && Objects.equals(ocean[x][y - 1].getSquareStatus().toString(), "SHIP"))
+                || (input.validateCords(x, y + 1) && Objects.equals(ocean[x][y + 1].getSquareStatus().toString(), "SHIP"));
     }
 
     public boolean checkEdgeNeighbours(Board board, int x, int y) {
         Square[][] ocean = board.getOcean();
-        return (checkIfValidCords(x - 1, y - 1) && Objects.equals(ocean[x - 1][y - 1].getSquareStatus().toString(), "SHIP"))
-                || (checkIfValidCords(x - 1, y + 1) && Objects.equals(ocean[x - 1][y + 1].getSquareStatus().toString(), "SHIP"))
-                || (checkIfValidCords(x + 1, y - 1) && Objects.equals(ocean[x + 1][y - 1].getSquareStatus().toString(), "SHIP"))
-                || (checkIfValidCords(x + 1, y + 1) && Objects.equals(ocean[x + 1][y + 1].getSquareStatus().toString(), "SHIP"));
+        return (input.validateCords(x - 1, y - 1) && Objects.equals(ocean[x - 1][y - 1].getSquareStatus().toString(), "SHIP"))
+                || (input.validateCords(x - 1, y + 1) && Objects.equals(ocean[x - 1][y + 1].getSquareStatus().toString(), "SHIP"))
+                || (input.validateCords(x + 1, y - 1) && Objects.equals(ocean[x + 1][y - 1].getSquareStatus().toString(), "SHIP"))
+                || (input.validateCords(x + 1, y + 1) && Objects.equals(ocean[x + 1][y + 1].getSquareStatus().toString(), "SHIP"));
     }
 
 
-    public int[] getCoordinates() {
-        System.out.println("Please enter coordinates: ");
-        String move = input.getUserInput();
-        int x = input.convertInputIntoRow(move);
-        int y = input.convertInputIntoColumn(move);
-        if (checkIfValidCords(x, y)) {
-            return new int[]{x, y};
-        } else {
-            System.out.println("Wrong coordinates");
-            return getCoordinates();
-        }
-
-    }
-
-    public String getDirection() {
-        System.out.println("Horizontal(h) or vertical(v)? ");
-        String letter = input.getUserInput();
-        switch (letter) {
-            case "H", "V" -> {
-                return letter;
-            }
-        }
-        return getDirection();
-    }
 }
 

248ca3f755eb7774e48860d578aa35aa0372fcd2
Sylwester <tomczyk.sylwester@wp.pl>
2022-08-09 10:22:35 +0200

    player class

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,4 +1,14 @@
 package player;
 
+import board.Board;
+
+import java.util.List;
+
 public interface Player {
+
+
+
+    public boolean isAlive();
+
+
 }
--- src/main/java/player/UserPlayer.java
+++ src/main/java/player/UserPlayer.java
@@ -1,4 +1,22 @@
 package player;
 
+import board.Board;
+import ship.Ship;
+import java.util.*;
+
+
 public class UserPlayer implements Player{
+
+    private String playerName;
+
+    private Board board;
+
+    private List<Ship> playerShips = new ArrayList<>();
+    public boolean isAlive(){
+        return true;
+    }
+
+
+
+
 }
